9fd89d5af6f79f6eeaac9b421e1c846fec0920d1
M33kael <mickael.marty@viacesi.fr>
2017-05-30 22:51:41 +0200

    error

---
+++ Detect.py
@@ -0,0 +1,55 @@
+import cv2
+from glyphfunctions import *
+
+
+
+
+
+QUADRILATERAL_POINTS = 4
+SHAPE_RESIZE = 100.0
+BLACK_THRESHOLD = 100
+WHITE_THRESHOLD = 155
+GLYPH_PATTERN = [1, 0, 1, 0, 1, 0, 1, 0, 1]
+
+
+def detect_glyph (picture):
+
+    gray = cv2.cvtColor(picture, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edges = cv2.Canny(gray, 100, 200)
+    contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+
+    for contour in contours:
+        perimeter = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.01 * perimeter, True)
+
+        if len(approx) == QUADRILATERAL_POINTS:
+            topdown_quad = get_topdown_quad(gray, approx.reshape(4, 2))
+            resized_shape = resize_image(topdown_quad, SHAPE_RESIZE)
+            test = 5
+            test2 = 5
+
+            test = (resized_shape.shape[0] / 100.0) * 5
+            test2 = (resized_shape.shape[1] / 100.0) * 5
+
+            if resized_shape[test, test2] > BLACK_THRESHOLD: continue
+            glyph_found = False
+
+            for i in range(4):
+                glyph_pattern = get_glyph_pattern(resized_shape, BLACK_THRESHOLD, WHITE_THRESHOLD)
+
+                if glyph_pattern == GLYPH_PATTERN:
+                    glyph_found = True
+                    break
+
+                resized_shape = rotate_image(resized_shape, 90)
+
+            if glyph_found:
+
+                coord = approx.reshape(4, 2)
+                mid_x = (coord[0][0] - coord[2][0])/2
+                mid_y = (coord[0][1] - coord[2][1]) / 2
+
+                center = [mid_x, mid_y]
+                return center
--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import cv2
+from Detect import *
 
 cap = cv2.VideoCapture(0)
 pic = cv2.imread('test.jpg', 0)
@@ -11,6 +12,13 @@ while(True):
     # Display the resulting frame
     cv2.imshow('frame', frame)
 
+    #Bout a changer
+    frame_straight = frame
+
+    position_cross = detect_glyph(frame_straight)
+    pic = np.zeros( (len(frame_straight), len(frame_straight[0]), 3), np.uint8)
+    if position_cross is not None:
+        pic[position_cross[0], position_cross[1]] = (255, 0, 0)
 
     #Vanilla image to display
     cv2.imshow('Picture', pic)
@@ -23,6 +31,7 @@ while(True):
     cv2.namedWindow('Projector', 0)
     cv2.setWindowProperty('Projector',  0, cv2.cv.CV_WINDOW_FULLSCREEN)
     cv2.imshow('Projector', transform)
+
 
 
 
